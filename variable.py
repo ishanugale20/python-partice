bfff2bfecafec208425c1510dcd2a401848a344a
ishanugale20 <ishanugale20@gmail.com>
2026-06-24 15:54:58 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,15 @@
+name = "nakul"
+print(name)
+print(type(name))
+
+age = 23
+print(age)
+print(type(age))
+ 
+cgpa = 9.9
+print (cgpa)
+print(type(cgpa))
+
+__echoflux = True
+print(__echoflux)
+print(type(__echoflux))
