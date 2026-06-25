a87f6174081a76ec92e167bdcc9a4715f8e34759
ishanugale20 <ishanugale20@gmail.com>
2026-06-25 20:59:40 +0530

    Escape Sequence

---
+++ esc.py
@@ -0,0 +1,8 @@
+print("hello world \ni am ishan\ni want job") #here \n add newline but you have to use in ("\n")
+print("i am good boy\ni am superman\ti want job") #here \t use as give space here 
+
+print("hello\"world")#here \" use to give 
+
+print("hello world","i want job 123") #here , is space
+print("hello world","i want job",sep=".") # here sep is space filler
+print("hello world","i want job 123",sep="<>",end="..")# here end is end representer 
