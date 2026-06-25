0019624540079e20abc322e83b5543c6de31eded
ishanugale20 <ishanugale20@gmail.com>
2026-06-25 13:29:46 +0530

    hello world,typecasting

---
+++ typecasting.py
@@ -0,0 +1,54 @@
+age = "34"
+print(age)
+print(type(age))
+
+age_int = int(age)
+print(age_int)
+print(type(age_int))
+
+age = 43
+print(age)
+print(type(age))
+
+age_str = str(age)
+print(age_str)
+print(type(age_str))
+
+age= 34
+print(age)
+print(type(age))
+age_str = str(age)
+print(type(age_str))
+
+age = "34" 
+print(age)
+print(type(age))
+
+age_int = int(age)
+print (age_int)
+print(type(age_int))
+
+pi = 3.4
+print(pi)
+print(type(pi))
+ 
+int_pi = int(pi)
+print(int_pi)
+print(type(int_pi))
+
+mission = True 
+print(mission)
+print(type(mission))
+
+string_mission = str(mission)
+print(string_mission)
+print(type(string_mission))
+
+name = "3.4" 
+print(name)
+print(type(name))
+
+name_flote = float(name)
+print(name_flote)
+print(type(name_flote))
+
