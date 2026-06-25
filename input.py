7ce5367615639663b52ce99bc23b8efb5ef8ad6e
ishanugale20 <ishanugale20@gmail.com>
2026-06-25 14:00:53 +0530

    input

---
+++ input.py
@@ -0,0 +1,21 @@
+a = input("first number")
+print(a)
+ 
+b= input("enter your name")
+print(b)
+
+# this is normal input now we will do it with twist 
+
+a= input ("enter first number")
+a= int(a) #here we can use another method also it will show in other eg down
+ 
+b= input("enter second number")
+b= int(b) #here we can use another method also it will show in other eg down
+
+print(a+b)
+print(a-b)
+
+a= int(input("enter first number")) #here we have directly uses int typecasting so we can use both also 
+b= int(input("enter sceond number")) #here we have directly uses int typecasting so we can use both also 
+
+print(a+b)
