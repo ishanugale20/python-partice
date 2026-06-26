5c86041f50d675ca1fd6dce84f8d6e9efe3472d2
ishanugale20 <ishanugale20@gmail.com>
2026-06-26 21:30:34 +0530

    friday pratice

---
+++ particeidk.py
@@ -0,0 +1,12 @@
+print("hello sir'")
+a=input("enter your age")
+a=int(a)
+b=input("enter your wife age")
+b=int(b)
+print(a+b)
+
+
+print("ishan wants job\nbut are are the one who can give me")
+print("ishan\tugale")
+print("ishan want job","can someone give it to me",sep="...")
+print("can some one give me\nplz give me job",end="!!!")
