ac5cc7c3ae17e5f1ab53bbbaec93db95f637b1cf
ishanugale20 <ishanugale20@gmail.com>
2026-06-27 18:10:20 +0000

    i was busy today

---
+++ partice_idk2.py
@@ -0,0 +1,12 @@
+age_int = int(age)
+print(age_int)
+print(type(age_int))
+
+age = 43
+print(age)
+print(type(age))
+
+print("ishan wants job\nbut are are the one who can give me")
+print("ishan\tugale")
+print("ishan want job","can someone give it to me",sep="...")
+print("can some one give me\nplz give me job",end="!!!")
