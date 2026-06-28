c169488da3e1b710ad72a99f6ff8fa2bc401003b
ishanugale20 <ishanugale20@gmail.com>
2026-06-28 22:59:46 +0530

    operators

---
+++ Operators.py
@@ -0,0 +1,63 @@
+# Arithmetic Operators
+a=34
+b=2
+
+# -----> + (Addition), - (Subtraction), * (Multiplication), / (Division), % (Modulus), ** (Exponentiation), // (Floor Division)
+print("a+b= ",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a//b=",a//b) 
+print("a%b=",a%b) #calulating remender is called as Modulus
+
+# Comparison Operators
+# -----> == (Equal), != (Not Equal), > (Greater Than), < (Less Than), >= (Greater Than or Equal), <= (Less Than or Equal).
+print(a>36)
+print(a<36)
+print(a<=34)
+print(a>=24) 
+print(a==34)#is a eqale to 34 ?
+print(a==35)#is a eqale to 35 ?
+print(a!=36)#is a not eqale to 36 
+
+# Logical Operators
+# -----> and, or, not
+
+a=True
+b=False
+
+print(True and False) #and may t-t he true hota hay baki false
+print(False and True)
+print(True and True)
+print(False and False)
+
+print(True or False) #iss may f-f hi false hota hay 
+print(False or True)
+print(True or True)
+print(False or False)
+
+print(not(False))
+print(not(True))
+
+# Assignment Operators
+# -----> =, +=, -=, *=, /=, %=, **=, //=
+
+a=23
+
+print(a)
+a-=3.      #we can directly add minus or mutiply and diff at same time
+print(a)
+a+=3
+print(a)
+a*=3
+print(a)
+a/=3
+print(a)
+a%=3
+print(a)
+a**=3
+print(a)
+a//=3
+
+
+
