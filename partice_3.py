51a38fa986717fabc6e581388ab2af209f629971
ishanugale20 <ishanugale20@gmail.com>
2026-06-30 23:05:14 +0530

    partice_3

---
+++ partice_3.py
@@ -0,0 +1,47 @@
+# # 1>
+print("Hello, World! Welcome to Python.")
+
+#2>
+print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+
+#4>
+a="45" #string
+a=int(a)
+print(type(a))
+
+# #5> 
+b= input("Wow! I also like <food>.")
+print(b)
+
+# 3>
+a="ishan"
+print(a)
+print(type(a))
+
+b=3
+print(b)
+print(type(b))
+
+cgpa=9.8
+print(cgpa)
+print(type(cgpa))
+
+is_student=True
+print(is_student)
+
+#6>
+a=int(34)
+b=int(23)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#Q7>
+print("Hello \"Python\" World!\tThis is on a new line.")
+
+#Q8>
+a=8
+print("a*a")
+print()
